dd32da6c47cf68187c5990102178090c2e893d58
noxx92 <dejan-h@live.de>
2018-09-23 18:43:22 +0200

    Add files via upload

---
+++ sass-watch-wp.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "Creating Sass Watches.\n"
+files=( * )
+
+for file in "${files[@]}"; do
+    extension="${file##*.}"
+	fullname="$file"
+	filename="${file%.*}"
+	
+	if [ "$extension" == "scss" ]; then
+        echo "watching: $fullname"
+        if [ "$filename" == "style" ]; then
+            sass --watch $filename.scss:../$filename.css &
+        else
+            sass --watch $filename.scss:../css/$filename.css &
+        fi
+    fi
+	
+done
