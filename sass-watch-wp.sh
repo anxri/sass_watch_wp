3840de4f47a15872115c82760e6269bb8d75ebbb
noxx92 <dejan-h@live.de>
2019-06-04 18:47:56 +0200

    Update sass-watch-wp.sh

--- sass-watch-wp.sh
+++ sass-watch-wp.sh
@@ -3,17 +3,17 @@ echo "Creating Sass Watches.\n"
 files=( * )
 
 for file in "${files[@]}"; do
-    extension="${file##*.}"
+	extension="${file##*.}"
 	fullname="$file"
 	filename="${file%.*}"
 	
 	if [ "$extension" == "scss" ]; then
-        echo "watching: $fullname"
-        if [ "$filename" == "style" ]; then
-            sass --watch $filename.scss:../$filename.css &
-        else
-            sass --watch $filename.scss:../css/$filename.css &
-        fi
-    fi
+        	echo "watching: $fullname"
+		if [ "$filename" == "style" ]; then
+			sass --watch $filename.scss:../$filename.css &
+		else
+			sass --watch $filename.scss:../css/$filename.css &
+		fi
+	fi
 	
 done
